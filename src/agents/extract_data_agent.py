815b15f409a97c311c154f17f3b9a6f70b200fd4
Almuth Meier <almuth.meier@schott.com>
2025-02-25 15:55:54 +0100

    feat: file for data extraction agent

---
+++ src/agents/extract_data_agent.py
@@ -0,0 +1,9 @@
+
+
+
+def construct_extract_data():
+    """Constructs and returns the state graph for extracting data from step 1."""
+    workflow = StateGraph(ExtractMainDataState)
+    graph = None
+
+    return graph
